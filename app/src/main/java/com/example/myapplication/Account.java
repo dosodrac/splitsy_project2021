87b94d405cb0bbae0739072df6439dde5f805f46
Bishal <bishalrai111@yahoo.com>
2021-02-17 15:53:12 +0000

    added botom navigation

--- app/src/main/java/com/example/myapplication/Account.java
+++ app/src/main/java/com/example/myapplication/Account.java
@@ -1,27 +1,46 @@
 
 package com.example.myapplication;
 
+        import androidx.annotation.NonNull;
         import androidx.appcompat.app.AppCompatActivity;
+        import androidx.fragment.app.Fragment;
+        import androidx.fragment.app.FragmentTransaction;
 
         import android.content.Intent;
         import android.os.Bundle;
+        import android.view.Menu;
+        import android.view.MenuItem;
         import android.view.View;
         import android.widget.Button;
         import android.widget.TextView;
 
+        import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+        import java.net.CacheRequest;
+
 public class Account extends AppCompatActivity {
     Button logout;
-    TextView editpass;
+    TextView changepassword;
+
 
     //addcard
     TextView addcard;
+
+    //navbar
+    BottomNavigationView bottomNavigation;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_account);
 
+
+        bottomNavigation = findViewById(R.id.bottom_navigation);
+        bottomNavigation.setOnNavigationItemSelectedListener(navigationItemSelectedListener);
+
         logout=findViewById(R.id.logoutbtn);
+
 
         logout.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -33,9 +52,9 @@ public class Account extends AppCompatActivity {
 
 
         //editpassword
-        editpass=findViewById(R.id.changepassword);
+        changepassword=findViewById(R.id.changepassword);
 
-        editpass.setOnClickListener(new View.OnClickListener() {
+        changepassword.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent intent=new Intent(Account.this, EditPassword.class);
@@ -57,4 +76,33 @@ public class Account extends AppCompatActivity {
         });
 
     }
+
+    public void openFragment(Fragment fragment) {
+        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+        transaction.replace(R.id.container, fragment);
+        transaction.addToBackStack(null);
+        transaction.commit();
+    }
+
+    BottomNavigationView.OnNavigationItemSelectedListener navigationItemSelectedListener =
+            new BottomNavigationView.OnNavigationItemSelectedListener() {
+                @Override public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                    switch (item.getItemId()) {
+                        case R.id.nav_home:
+                            Intent i=new Intent(Account.this,MainActivity.class);
+                            startActivity(i);
+
+                            return true;
+                        case R.id.nav_profile:
+                            Intent intent=new Intent(Account.this,AddCard.class);
+                            startActivity(intent);
+                            return true;
+                        case R.id.nav_setting:
+                            Intent j =new Intent(Account.this,PastBill.class);
+                            startActivity(j);
+                            return true;
+                    }
+                    return false;
+                }
+            };
 }
