528766044824bbc9fb888ea1b2da3d9f0a0ddf31
Bishal <bishalrai111@yahoo.com>
2021-02-17 18:41:56 +0000

    added files

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,26 +1,53 @@
 package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
     Button pro;
+
+    //bill
+    Button bill;
+
+
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+
+
         pro=findViewById(R.id.pro);
+
+        //bill
+        bill=findViewById(R.id.bill);
+
 
         pro.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent intent=new Intent(MainActivity.this, Account.class);
+                startActivity(intent);
+            }
+        });
+
+        //bill
+
+        bill.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent=new Intent(MainActivity.this, PastBill.class);
                 startActivity(intent);
             }
         });
