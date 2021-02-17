eaea28d79ae259b45eceb4133155ddd6c6ba0131
Yasmin Paksoy <yaspaksoy@icloud.com>
2021-02-17 18:39:49 +0000

    Enter bill details for equally added.

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -9,6 +9,6 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.create_new_bill);
+        setContentView(R.layout.enter_bill_details);
     }
 }
