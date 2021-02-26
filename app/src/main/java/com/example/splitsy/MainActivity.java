94c3bb1dd05a957aa6ed66c73c5e8c01707c6e7f
Yasmin Paksoy <yaspaksoy@icloud.com>
2021-02-26 00:43:38 +0000

    Consistency editing.

--- app/src/main/java/com/example/splitsy/MainActivity.java
+++ app/src/main/java/com/example/splitsy/MainActivity.java
@@ -10,7 +10,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.account_change_password);
+        setContentView(R.layout.change_password);
 
 
 
