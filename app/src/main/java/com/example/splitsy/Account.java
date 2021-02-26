d4dcee1e43de74a579fc58877cb7c30ecb864c96
Yasmin Paksoy <yaspaksoy@icloud.com>
2021-02-26 19:02:31 +0000

    Consistency changes.

--- app/src/main/java/com/example/splitsy/Account.java
+++ app/src/main/java/com/example/splitsy/Account.java
@@ -28,7 +28,7 @@ public class Account extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_account);
+        setContentView(R.layout.your_account);
 
         //bottom navbar
         bottomNavigation = findViewById(R.id.bottom_navigation);
