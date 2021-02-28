adfc336750e08a74a0f92651dc684af51984c231
Epimetheus <57752800+44617669640D@users.noreply.github.com>
2021-02-28 18:02:45 +0000

    XML consistency check

--- app/src/main/java/com/example/splitsy/MainActivity.java
+++ app/src/main/java/com/example/splitsy/MainActivity.java
@@ -10,20 +10,9 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-<<<<<<< HEAD:app/src/main/java/com/example/myapplication/MainActivity.java
-        setContentView(R.layout.your_bill_member);
-=======
         setContentView(R.layout.change_password);
-
-
-
->>>>>>> XMLpart2:app/src/main/java/com/example/splitsy/MainActivity.java
     }
-
-
-
 }
-
 
 
 
