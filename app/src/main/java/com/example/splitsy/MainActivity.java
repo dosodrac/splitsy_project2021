4c86226124c2a8f98b44a907417f8360e0f50c25
Yasmin Paksoy <yaspaksoy@icloud.com>
2021-02-28 23:37:40 +0000

    Merge error checking

--- app/src/main/java/com/example/splitsy/MainActivity.java
+++ app/src/main/java/com/example/splitsy/MainActivity.java
@@ -6,13 +6,9 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.change_password);
     }
 }
-
-
-
