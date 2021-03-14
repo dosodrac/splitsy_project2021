ca17f929ffeb81d6969408355219f1754e5d0d21
MarySousa001 <78039209+MarySousa001@users.noreply.github.com>
2021-03-14 09:11:28 +0000

    share code created

--- app/src/main/java/com/softwareTeam/splitsy/ShareCode.java
+++ app/src/main/java/com/softwareTeam/splitsy/ShareCode.java
@@ -6,18 +6,15 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.MenuItem;
-import android.widget.Button;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
-public class EnterBillDetails extends AppCompatActivity {
-
-    Button createbtn; // enter bill details button create
+public class ShareCode extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_enter_bill_details);
+        setContentView(R.layout.activity_share_code);
 
         // ---------Bottom navigation code
         BottomNavigationView bottomNavigationView = findViewById(R.id.nav_bottom);
@@ -28,22 +25,17 @@ public class EnterBillDetails extends AppCompatActivity {
             public boolean onNavigationItemSelected(@NonNull MenuItem item) {
                 switch (item.getItemId()){
                     case R.id.pastBills:
-                        startActivity(new Intent(EnterBillDetails.this, Past_Bills.class));
+                        startActivity(new Intent(ShareCode.this, Past_Bills.class));
                         return true;
                     case R.id.newBill:
-                        startActivity(new Intent(EnterBillDetails.this, New_Bill.class));
+                        startActivity(new Intent(ShareCode.this, New_Bill.class));
                         return true;
                     case R.id.account:
-                        startActivity(new Intent(EnterBillDetails.this, Your_Account.class));
+                        startActivity(new Intent(ShareCode.this, Your_Account.class));
                         return true;
                 }
                 return false;
             }
         });// --------end of Botton navigation
-
-        //----create button------
-        createbtn = (Button) findViewById(R.id.createButton);
-        startActivity(new Intent(EnterBillDetails.this,ShareCode.class));
-        // ---end of create button-----
     }
 }
