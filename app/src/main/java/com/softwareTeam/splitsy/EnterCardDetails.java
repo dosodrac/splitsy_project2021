2791f02f06064fa598f3575e89735d1cd98c397e
MarySousa001 <78039209+MarySousa001@users.noreply.github.com>
2021-03-14 17:22:11 +0000

    enter card details created

--- app/src/main/java/com/softwareTeam/splitsy/EnterCardDetails.java
+++ app/src/main/java/com/softwareTeam/splitsy/EnterCardDetails.java
@@ -2,13 +2,26 @@ package com.softwareTeam.splitsy;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class EnterCardDetails extends AppCompatActivity {
+
+    Button finishbtn; // finish button
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_enter_card_details);
+
+        finishbtn = (Button)findViewById(R.id.finishButton);
+        finishbtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(EnterCardDetails.this, Sign_In.class));
+            }
+        });
     }
 }
