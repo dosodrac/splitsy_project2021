5352ea75d4a0a5ca01d39ba0b8e8bb6e07819dcb
MarySousa001 <78039209+MarySousa001@users.noreply.github.com>
2021-03-14 16:26:35 +0000

    Account details created

--- app/src/main/java/com/softwareTeam/splitsy/AccountDetails.java
+++ app/src/main/java/com/softwareTeam/splitsy/AccountDetails.java
@@ -2,13 +2,26 @@ package com.softwareTeam.splitsy;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class AccountDetails extends AppCompatActivity {
+
+    Button continuebtn; //account continue button
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_account_details);
+
+        continuebtn = (Button)findViewById(R.id.accountContinueButton);
+        continuebtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(AccountDetails.this, EnterCardDetails.class));
+            }
+        });
     }
 }
