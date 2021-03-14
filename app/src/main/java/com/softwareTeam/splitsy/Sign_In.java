9e72bbc69e5e9e267c7bd4ec81b8a9dddc5792b5
MarySousa001 <78039209+MarySousa001@users.noreply.github.com>
2021-03-14 14:36:29 +0000

    filter menu created1

--- app/src/main/java/com/softwareTeam/splitsy/Sign_In.java
+++ app/src/main/java/com/softwareTeam/splitsy/Sign_In.java
@@ -6,22 +6,44 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class Sign_In extends AppCompatActivity {
 
     Button btnsingIn;
+    TextView signup;
+    ImageView signupimg;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.sign_in);
 
+        // singIn button code--------------
         btnsingIn = (Button)findViewById(R.id.signInButton);
         btnsingIn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 startActivity(new Intent(Sign_In.this, Enable_Fingerprint.class));
             }
+        });// end sign in button------------------------
+
+        //-------------------------sign up link
+        signup = (TextView)findViewById(R.id.signUpText);
+        signup.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(Sign_In.this, SignUp.class));
+            }
         });
+        signupimg = (ImageView) findViewById(R.id.signInImage);
+        signupimg.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(Sign_In.this, SignUp.class));
+            }
+        });// -------------------end of sign up link
+
     }
 }
