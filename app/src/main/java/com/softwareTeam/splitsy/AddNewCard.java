642f5bf1902f0136416678d2f60564de569980d6
MarySousa001 <78039209+MarySousa001@users.noreply.github.com>
2021-03-15 00:44:12 +0000

    bank card

--- app/src/main/java/com/softwareTeam/splitsy/AddNewCard.java
+++ app/src/main/java/com/softwareTeam/splitsy/AddNewCard.java
@@ -2,13 +2,27 @@ package com.softwareTeam.splitsy;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class AddNewCard extends AppCompatActivity {
+
+    Button finishbtn; //finish button
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_new_card);
+
+        //---finish button----
+        finishbtn = (Button)findViewById(R.id.finishButton);
+        finishbtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(AddNewCard.this, BankCardAdded.class));
+            }
+        });
     }
 }
