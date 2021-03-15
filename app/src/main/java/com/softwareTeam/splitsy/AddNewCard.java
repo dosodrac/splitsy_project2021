77ac8b87980a0272ced2b6f4e484f94b9f278429
MarySousa001 <78039209+MarySousa001@users.noreply.github.com>
2021-03-15 00:07:22 +0000

    account updates

---
+++ app/src/main/java/com/softwareTeam/splitsy/AddNewCard.java
@@ -0,0 +1,14 @@
+package com.softwareTeam.splitsy;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AddNewCard extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_new_card);
+    }
+}
--- app/src/main/java/com/softwareTeam/splitsy/Your_Account.java
+++ app/src/main/java/com/softwareTeam/splitsy/Your_Account.java
@@ -9,6 +9,7 @@ import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
@@ -16,6 +17,9 @@ public class Your_Account extends AppCompatActivity {
 
     Button logoutbtn; // log out button
     ImageView settingbtn; //settings button
+    TextView change_email_link; // change email link
+    TextView change_passw_link; // change password link
+    TextView add_card_link; // edit card link
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -59,7 +63,40 @@ public class Your_Account extends AppCompatActivity {
             public void onClick(View v) {
                 startActivity(new Intent(Your_Account.this, Settings.class));
             }
-        });
+        });//  end settings button---------
+
+        //----change email link-----
+        change_email_link = (TextView)findViewById(R.id.changeEmailText);
+        change_email_link.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(Your_Account.this, ChangeEmail.class));
+
+            }
+        });//  ---- end change email link----
+
+        //-----change password link-----
+        change_passw_link = (TextView)findViewById(R.id.changePasswordText);
+        change_passw_link.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(Your_Account.this, ChangePassword.class));
+
+
+            }
+        });// end change password link-------
+
+        //----add card link----
+        add_card_link = (TextView)findViewById(R.id.addCardText);
+        add_card_link.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(Your_Account.this, AddNewCard.class));
+
+
+            }
+        });//-----end add card link
+
 
     }
 }
