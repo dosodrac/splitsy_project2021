4a901b65396d081cd7c45ce94aaa8082734d8b46
Yasmin Paksoy <yaspaksoy@icloud.com>
2021-03-20 00:20:31 +0000

    Added SQL connection code

--- app/src/main/java/com/softwareTeam/splitsy/Sign_In.java
+++ app/src/main/java/com/softwareTeam/splitsy/Sign_In.java
@@ -2,17 +2,30 @@ package com.softwareTeam.splitsy;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
 public class Sign_In extends AppCompatActivity {
 
+    //Input buttons
+    private EditText emailInput;
+    private EditText passwordInput;
+    private TextView info;
+
     Button btnsingIn; // sign in button
-    TextView signup;// sign up linl
+    TextView signup;// sign up link
     ImageView signupimg; // sign up link
 
     TextView forgotlink; // forgot password link
@@ -24,11 +37,16 @@ public class Sign_In extends AppCompatActivity {
         setContentView(R.layout.sign_in);
 
         // singIn button code--------------
+        emailInput = (EditText)findViewById(R.id.emailInput);
+        passwordInput = (EditText)findViewById(R.id.passwordInput);
+        info = (TextView)findViewById(R.id.loginInfo);
+
         btnsingIn = (Button)findViewById(R.id.signInButton);
         btnsingIn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                startActivity(new Intent(Sign_In.this, Enable_Fingerprint.class));
+                new Task().execute();
+                //startActivity(new Intent(Sign_In.this, Enable_Fingerprint.class));
             }
         });// end sign in button------------------------
 
@@ -67,4 +85,39 @@ public class Sign_In extends AppCompatActivity {
         });// --------------end of forgot links-----------------
 
     }
+
+    class Task extends AsyncTask<Void, Void, Void> {
+        /* String records = "", error=""; */
+
+        @SuppressLint("SetTextI18n")
+        @Override
+        protected Void doInBackground(Void... voids){
+            try{
+                Class.forName("com.mysql.jdbc.Driver");
+                Connection connection = DriverManager.getConnection("jdbc:mysql://igor.gold.ac.uk:3307/dcard001_splitsy", "dcard001", "134114");
+                Statement statement = connection.createStatement();
+                ResultSet resultSet = statement.executeQuery("SELECT * FROM user WHERE email='" + emailInput + "'");
+
+                String password = passwordInput.getText().toString();
+
+                while(resultSet.next()) {
+                    if(resultSet.getString(3).equals(password)){
+                        //Intent i = new Intent(MainActivity.this,Account.class);
+                        //startActivity(i);
+
+                        startActivity(new Intent(Sign_In.this, Enable_Fingerprint.class));
+                    }
+                    else{
+                        info.setText("There was an error, please try again");
+                        startActivity(new Intent(Sign_In.this, Sign_In.class));
+                    }
+                }
+            }catch(Exception e){
+                info.setText("There was an error, please try again.");
+            }
+            return null;
+        }
+    }
+
+
 }
