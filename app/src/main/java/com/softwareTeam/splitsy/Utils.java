a3868787c077438dd47301194478bae1b9b7dc9a
MarySousa001 <78039209+MarySousa001@users.noreply.github.com>
2021-03-21 19:57:50 +0000

    Unit test in email and password

--- app/src/main/java/com/softwareTeam/splitsy/Utils.java
+++ app/src/main/java/com/softwareTeam/splitsy/Utils.java
@@ -9,7 +9,7 @@ public class Utils {
 
 
     public static boolean checkEmailForValidity(String email) {
-       // email = email.trim();
+        email = email.trim();
 
         Matcher matcher = VALID_EMAIL_ADDRESS_REGEX.matcher(email);
         return matcher.find();
@@ -20,5 +20,17 @@ public class Utils {
             Pattern.compile("^[A-Z0-9._%+-]+@[A-Z0-9.-]+\\.[A-Z]{2,6}$", Pattern.CASE_INSENSITIVE);
 
 
+        private static final String VALID_PASSWORD_REGEX = "((?=.*\\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[@#$%=:\\?]).{8,12})";
 
-}
+        /**
+         * Returns true if password matches the validation rules
+         *
+         * @param password
+         * @return
+         */
+        public static boolean validatePassword(String password) {
+            return (password != null && password.matches(VALID_PASSWORD_REGEX));
+        }
+    }
+
+
--- app/src/test/java/com/softwareTeam/splitsy/UtilsTest.java
+++ app/src/test/java/com/softwareTeam/splitsy/UtilsTest.java
@@ -7,19 +7,122 @@ import org.junit.Test;
 
 import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
 
 public class UtilsTest {
 
     @Test
     public void testIsEmailValid() {
         String testEmail = "anupamchugh@gmail.com";
-        Assert.assertThat(String.format("Email Validity Test failed for %s ", testEmail), Utils.checkEmailForValidity(testEmail), is(true));
+        Assert.assertThat(String.format("Email Validity Test failed for %s ", testEmail),
+                Utils.checkEmailForValidity(testEmail), is(true));
     }
 
     @Test
     public void testEmailValidityPartTwo() {
-        String testEmail = "   anupamchugh@gmail.com  ";
-        Assert.assertThat(String.format("Email Validity Test failed for %s ", testEmail), Utils.checkEmailForValidity(testEmail), is(true));
+        String testEmail = "    anupamchugh@gmail.com    ";
+        Assert.assertThat(String.format("Email Validity Test failed for %s ", testEmail),
+                Utils.checkEmailForValidity(testEmail), is(true));
     }
+    @Test
+    public void validatePassword_Null() {
+        // setup
+        String password = null;
+
+        // execute
+        boolean actual = Utils.validatePassword(password);
+
+        // assert
+        assertFalse(actual);
+    }
+
+    @Test
+    public void validatePassword_EmptyString() {
+        // setup
+        String password = "";
+
+        // execute
+        boolean actual = Utils.validatePassword(password);
+
+        // assert
+        assertFalse(actual);
+    }
+
+    @Test
+    public void validatePassword_Missing_OneNumber() {
+        // setup
+        String password = "Abcdefg#";
+
+        // execute
+        boolean actual = Utils.validatePassword(password);
+
+        // assert
+        assertFalse(actual);
+    }
+
+    @Test
+    public void validatePassword_Missing_OneUpperCaseLetter() {
+        // setup
+        String password = "abcdefg5#";
+
+        // execute
+        boolean actual = Utils.validatePassword(password);
+
+        // assert
+        assertFalse(actual);
+    }
+
+    @Test
+    public void validatePassword_Missing_OneLowerCaseLetter() {
+        // setup
+        String password = "ABCDEFG5#";
+
+        // execute
+        boolean actual = Utils.validatePassword(password);
+
+        // assert
+        assertFalse(actual);
+    }
+
+    @Test
+    public void validatePassword_Missing_OneSymbol() {
+        // setup
+        String password = "Abcdefg5";
+
+        // execute
+        boolean actual = Utils.validatePassword(password);
+
+        // assert
+        assertFalse(actual);
+    }
+
+    @Test
+    public void validatePassword_AllRulesMet() {
+        // setup
+        String password = "Abcdefg5#";
+
+        // execute
+        boolean actual = Utils.validatePassword(password);
+
+        // assert
+        assertTrue(actual);
+    }
+
+    @Test
+    public void validatePassword_LengthTooLong() {
+        // setup
+        String password = "Abcdefg5#abcdefgabcd";
+
+        // execute
+        boolean actual = Utils.validatePassword(password);
+
+        // assert
+        assertFalse(actual);
+    }
+    public static boolean validatePassword(String password) {
+        return true;
+    }
+
 
 }
